972be2a49f8c45eec72e5f195f1d923eadbddc2d
Fabio Neves <fabio@pop-os.localdomain>
2022-08-25 21:04:56 -0300

    feat: Created better script to start docker

---
+++ postgresql/index.sh
@@ -0,0 +1,15 @@
+source variables.sh
+
+docker stop ${POSTRES_CONTAINER_NAME} || true
+docker rm ${POSTRES_CONTAINER_NAME} || 
+
+echo "### STARTING POSTGRES... ###"
+docker run \
+  --name ${POSTRES_CONTAINER_NAME} \
+  --net ${DOCKER_NET_NAME} \
+  -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+  -p ${POSTGRES_PORT}:${POSTGRES_PORT} \
+  -v "$PWD/.volume/:/var/lib/postgresql/data" \
+  -d postgres
+
+echo "### POSTGRES RUNNING IN PORT: ${POSTGRES_PORT} ###"
