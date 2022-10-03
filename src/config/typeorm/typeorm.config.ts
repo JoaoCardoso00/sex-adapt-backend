6818bb6fab2a398b1dd403c8c919fff1cb298c31
Fabio Neves <fabio@pop-os.localdomain>
2022-10-02 21:56:01 -0300

    feat: typeorm config file

--- src/config/typeorm/typeorm.config.ts
+++ src/config/typeorm/typeorm.config.ts
@@ -1,11 +1,11 @@
-import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { join } from "path";
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
-	constructor(private configService: ConfigService) { }
+	constructor(private configService: ConfigService) {}
 	createTypeOrmOptions(): TypeOrmModuleOptions {
 		return {
 			type: 'postgres',
