c0793ccf6e8b00de66374f4d1fc375ec948f5ea8
Fabio Neves <fabio@pop-os.localdomain>
2022-10-14 12:55:06 -0300

    fix(lint): eslint fix

--- src/models/suport/dto/create-suport.dto.ts
+++ src/models/suport/dto/create-suport.dto.ts
@@ -1,7 +1,7 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateSuportDto {
-    @IsString({ message: "A mensagem deve ser um texto." })
-    @IsNotEmpty({ message: "A mensagem é obrigatória." })
-    message: string;
+	@IsString({ message: 'A mensagem deve ser um texto.' })
+	@IsNotEmpty({ message: 'A mensagem é obrigatória.' })
+	message: string;
 }
