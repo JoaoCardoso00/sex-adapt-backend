38425f1c73d2a804f298df3c973a005734c280bc
Fabio Neves <fabio@pop-os.localdomain>
2022-10-14 12:54:31 -0300

    feat(DTO): created suport DTO

--- src/models/suport/dto/create-suport.dto.ts
+++ src/models/suport/dto/create-suport.dto.ts
@@ -1,1 +1,7 @@
-export class CreateSuportDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateSuportDto {
+    @IsString({ message: "A mensagem deve ser um texto." })
+    @IsNotEmpty({ message: "A mensagem é obrigatória." })
+    message: string;
+}
